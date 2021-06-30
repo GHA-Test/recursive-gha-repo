ff0de3e3deb9023fc6d263ea1a0eae17ba1c4280
Sean O'Mahoney <seandavidomahoney@gmail.com>
2021-06-30 13:17:30 +0100

    CHAOS

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "github" {}
+
+variable "commit_author" {
+  default = "GHA Test"
+}
+
+variable "commit_email" {
+  default = "seandavidomahoney+gha-test@gmail.com"
+}
+
+resource "random_string" "random" {
+  length           = 20
+  special          = false
+}
+
+resource "github_repository" "main" {
+  name       = "${ random_string.random.result }"
+}
+
+resource "github_repository_file" "tf" {
+  repository          = github_repository.main.name
+  file                = "main.tf"
+  content             = "main.tf"
+  commit_message      = "Added Terraform Config"
+  commit_author       = "${ var.commit_author }"
+  commit_email        = "${ var.commit_email }"
+  depends_on = [github_repository.main]
+}
+
+resource "github_repository_file" "gha" {
+  repository          = github_repository.main.name
+  file                = ".github/workflows/index.yaml"
+  content             = ".github/workflows/index.yaml"
+  commit_message      = "Added Terraform Config"
+  commit_author       = "${ var.commit_author }"
+  commit_email        = "${ var.commit_email }"
+  depends_on = [github_repository.main, github_repository_file.tf]
+}
